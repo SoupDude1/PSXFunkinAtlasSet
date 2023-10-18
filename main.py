72ebbd537e921a3ab7812a4a8d7c5c22fbe97dc5
IgorSou3000 <145292659+SoupDude1@users.noreply.github.com>
2023-10-18 14:25:28 -0300

    Added type hints
    1 - Added type hints to help peoples understand what type it should be.
    
    2 - Added a few constants to make the code more clear.
    
    3 - Made the main.py only import the necessary stuff of util.py instead of include everything.
    
    3 - Added comments and fixed the grammar of the existing ones

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 from PIL import Image
-from util import *
+from util import Rectangle, rectangles_are_equal, rectangles_collided, custom_round, compare_strings_ignore_trailing_numbers
 
 # Path to the spritesheet file
 SPRITESHEET_FILE_PATH = "example/BOYFRIEND"
@@ -9,35 +9,33 @@ BASE_IMAGE_NAME = "bf"
 # Scaling factor to resize the sprites
 SCALE_FACTOR = 0.25
 
+# Max image size
+MAX_WIDTH = 256
+MAX_HEIGHT = 256
+
 # Names of animations to be extracted from the XML file
 ANIMATION_NAMES = ["BF idle dance", "BF NOTE LEFT", "BF NOTE DOWN", "BF NOTE UP", "BF NOTE RIGHT"]
 
-# Function to load an image from a file
 def load_image(file_path):
-    try:
-        print(f"Loading {file_path}...")
-        print(f"Loaded!")
-        return Image.open(file_path)
+    png_file = Image.open(file_path)
+    print(f"Loading {file_path}...")
+    print("Loaded!")
 
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Failed to load {file_path}")
+    return png_file
 
-# Function to load XML data from a file
 def load_xml(file_path):
-    try:
-        print(f"Loading {file_path}...")
-        print(f"Loaded!")
-        return ET.parse(file_path).getroot()
+    xml_file = ET.parse(file_path).getroot()
+    print(f"Loading {file_path}...")
+    print("Loaded!")
 
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Failed to load {file_path}")
+    return xml_file
 
-# Extract the FNF sprite sheet XML attributes and scale their properties as needed
-def extract_sprite_attributes(xml_data):
-    sprite_attributes = []
+def extract_sprite_attributes(xml_data) -> list[Rectangle]:
+    sprite_attributes: list[Rectangle] = []
 
     for animation_name in ANIMATION_NAMES:
         for sub_texture in xml_data.findall("SubTexture"):
+            # Check if the provided XML animation name matches the current XML animation name
             if not compare_strings_ignore_trailing_numbers(animation_name, sub_texture.get("name")):
                 continue
 
@@ -49,122 +47,121 @@ def extract_sprite_attributes(xml_data):
                 height=custom_round(int(sub_texture.get("height")) * SCALE_FACTOR)
             )
 
-            # Check if the same frame exists in the XML list before appending
-            if not any(are_sprite_rectangles_equal(sprite_rect, existing_rect) for existing_rect in sprite_attributes):
-                sprite_attributes.append(sprite_rect)
+            # Check if the same frame exists in the XML list
+            if any(rectangles_are_equal(sprite_rect, existing_rect) for existing_rect in sprite_attributes):
+                continue
+
+            sprite_attributes.append(sprite_rect)
 
     return sprite_attributes
 
-# Function to trim sprites based on their attributes
-def trim_sprites(sprite_attributes, spritesheet):
-    trimmed_sprites = []
+def trim_sprites(xml_attributes: list[Rectangle], spritesheet: Image) -> list[Rectangle]:
+    sprites_rect: list[Rectangle] = []
 
-    for sprite_attr in sprite_attributes:
-        sprite = spritesheet.crop((sprite_attr.x, sprite_attr.y, sprite_attr.width + sprite_attr.x, sprite_attr.height + sprite_attr.y))
+    for attribute in xml_attributes:
+        # Crop the sprite using the XML list provided
+        sprite = spritesheet.crop((attribute.x, attribute.y, attribute.width + attribute.x, attribute.height + attribute.y))
 
         # Check if the sprite has valid content
         if sprite.getbbox() is None:
-            raise TypeError("Failed to pack sprites, check if your spritesheet has empty sprite spaces or if the XML scale is correct")
+            raise TypeError("Failed to pack sprites; check if your spritesheet has empty sprite spaces or if the XML scale is correct")
 
         bbox = sprite.getbbox()
 
         # Create a trimmed sprite rectangle
-        trimmed_sprite_rect = Rectangle(
-            x=sprite_attr.x + bbox[0],
-            y=sprite_attr.y + bbox[1],
+        sprite_rect = Rectangle(
+            x=attribute.x + bbox[0],
+            y=attribute.y + bbox[1],
             width=bbox[2] - bbox[0],
             height=bbox[3] - bbox[1]
         )
 
-        trimmed_sprites.append(trimmed_sprite_rect)
+        sprites_rect.append(sprite_rect)
 
-    return trimmed_sprites
+    return sprites_rect
 
-# Function to organize sprite positions for multiples images
-def organize_sprite_positions(trimmed_sprites):
-    sprite_positions = []
+def organize_sprite_positions(sprites_rect: list[Rectangle]) -> list[Rectangle]:
+    organized_sprites: list[Rectangle] = []
     current_pos_x = 0
     current_pos_y = 0
     current_counter = 0
 
-    for trimmed_sprite in trimmed_sprites:
-        # Create a sprite position rectangle
-        sprite_position = Rectangle(
-            x=trimmed_sprite.x,
-            y=trimmed_sprite.y,
-            width=trimmed_sprite.width,
-            height=trimmed_sprite.height,
+    for i in sprites_rect:
+        sprite_rect = Rectangle(
+            x=i.x,
+            y=i.y,
+            width=i.width,
+            height=i.height,
             counter=current_counter,
             pos_x=current_pos_x,
             pos_y=current_pos_y
         )
 
-        collided = False
-
         # Check for sprite position overflow in the X position
-        if sprite_position.pos_x + sprite_position.width > 256:
-            sprite_position.pos_x = 0
-            sprite_position.pos_y = 0
+        if sprite_rect.pos_x + sprite_rect.width > MAX_WIDTH:
+            sprite_rect.pos_x = 0
+            sprite_rect.pos_y = 0
 
         # Check for collisions with existing sprites
-        while any(rect.counter == sprite_position.counter and do_sprite_rectangles_collide(sprite_position, rect) for rect in sprite_positions):
-            sprite_position.pos_y += 1
-            collided = True
-
-        if collided:
-            sprite_position.pos_y += 1
+        while any(rect.counter == sprite_rect.counter and rectangles_collided(sprite_rect, rect) for rect in organized_sprites):
+            sprite_rect.pos_y += 1
 
         # Check for sprite position overflow in the Y position
-        if sprite_position.pos_y + sprite_position.height > 256:
-            sprite_position.pos_x = sprite_position.pos_y = 0
-            sprite_position.counter += 1
+        if sprite_rect.pos_y + sprite_rect.height > MAX_HEIGHT:
+            sprite_rect.pos_x = sprite_rect.pos_y = 0
+            sprite_rect.counter += 1
 
-        sprite_positions.append(sprite_position)
+        organized_sprites.append(sprite_rect)
 
-        current_pos_x = sprite_position.pos_x
-        current_pos_y = sprite_position.pos_y
-        current_counter = sprite_position.counter
+        current_pos_x = sprite_rect.pos_x
+        current_pos_y = sprite_rect.pos_y
+        current_counter = sprite_rect.counter
 
-        current_pos_x += sprite_position.width + 1
+        current_pos_x += sprite_rect.width + 1
         current_pos_y = 0
 
-    return sprite_positions
+    return organized_sprites
 
-# Function to pack sprites into multiples images or only a single one
-def pack_sprites(spritesheet, sprite_positions):
+# Function to pack sprites into multiple images or only a single one
+def pack_sprites(spritesheet: Image, sprite_positions: list[Rectangle]):
     current_counter = 0
-    current_image = Image.new("RGBA", (256, 256))
+    current_image = Image.new("RGBA", (MAX_WIDTH, MAX_HEIGHT))
 
-    for sprite_position in sprite_positions:
+    for sprite in sprite_positions:
         image_name = f"{BASE_IMAGE_NAME}{str(current_counter)}.png"
 
-        sprite = spritesheet.crop((sprite_position.x, sprite_position.y, sprite_position.width + sprite_position.x, sprite_position.height + sprite_position.y))
+        sprite_image = spritesheet.crop((sprite.x, sprite.y, sprite.width + sprite.x, sprite.height + sprite.y))
 
-        # Check if the sprite counter has changed
-        if current_counter != sprite_position.counter:
+        # Check if the sprite counter has changed, and if it does save the image and create another one
+        if current_counter != sprite.counter:
             current_image.save(image_name)
-            current_counter = sprite_position.counter
-            current_image = Image.new("RGBA", (256, 256))
+            current_counter = sprite.counter
+            current_image = Image.new("RGBA", (MAX_WIDTH, MAX_HEIGHT))
             print(f"Packing sprite: {image_name}")
 
-        # Check if the current sprite is the last one
-        if sprite_position == sprite_positions[-1]:
+        # Check if the current sprite is the last one, and if it does force to save the image to avoid bugs
+        if sprite == sprite_positions[-1]:
             image_name = f"{BASE_IMAGE_NAME}{str(current_counter)}.png"
-            current_image.paste(sprite, (sprite_position.pos_x, sprite_position.pos_y))
+            current_image.paste(sprite_image, (sprite.pos_x, sprite.pos_y))
             current_image.save(image_name)
             print(f"Packing sprite: {image_name}")
 
-        current_image.paste(sprite, (sprite_position.pos_x, sprite_position.pos_y))
+        current_image.paste(sprite_image, (sprite.pos_x, sprite.pos_y))
 
-if __name__ == "__main__":
+
+def main():
     spritesheet_image = load_image(SPRITESHEET_FILE_PATH + ".png")
+    xml_data = load_xml(SPRITESHEET_FILE_PATH + ".xml")
 
     if spritesheet_image.mode == "P":
         print("Warning: Image is indexed; converting to RGBA")
         spritesheet_image = spritesheet_image.convert(mode="RGBA")
 
-    xml_data = load_xml(SPRITESHEET_FILE_PATH + ".xml")
     sprite_attributes_list = extract_sprite_attributes(xml_data)
     trimmed_sprite_attributes = trim_sprites(sprite_attributes_list, spritesheet_image)
     organized_sprite_positions = organize_sprite_positions(trimmed_sprite_attributes)
     pack_sprites(spritesheet_image, organized_sprite_positions)
+
+
+if __name__ == "__main__":
+    main()
--- util.py
+++ util.py
@@ -2,28 +2,28 @@ import re
 
 class Rectangle:
     def __init__(self, x=0, y=0, width=0, height=0, counter=0, pos_x=0, pos_y=0):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.counter = counter
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        self.x : int = x
+        self.y : int = y
+        self.width : int = width
+        self.height : int = height
+        self.counter : int = counter
+        self.pos_x : int = pos_x
+        self.pos_y : int = pos_y
 
-def are_sprite_rectangles_equal(rect1, rect2):
+def rectangles_are_equal(rect1 : Rectangle, rect2 : Rectangle) -> bool:
     return all(getattr(rect1, attr) == getattr(rect2, attr) for attr in ["x", "y", "width", "height"])
 
-def do_sprite_rectangles_collide(rect1, rect2):
+def rectangles_collided(rect1 : Rectangle, rect2 : Rectangle) -> bool:
     return (rect1.pos_x < rect2.pos_x + rect2.width and rect1.pos_x + rect1.width > rect2.pos_x and
             rect1.pos_y < rect2.pos_y + rect2.height and rect1.pos_y + rect1.height > rect2.pos_y)
 
-def custom_round(value):
+def custom_round(value : float):
     if value >= 0:
         return int(value + 0.5)
     else:
         return int(value - 0.5)
 
-def compare_strings_ignore_trailing_numbers(str1, str2):
+def compare_strings_ignore_trailing_numbers(str1 : str, str2 : str):
     str1_stripped = re.sub(r"\d+$", "", str1)
     str2_stripped = re.sub(r"\d+$", "", str2)
     return str1_stripped == str2_stripped
